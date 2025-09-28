ccbba74e735b346b85e15e88c16a8b7254c8c202
xinbinglim <xinbinglim@gmail.com>
2025-09-28 12:46:20 +0800

    change

--- ui.gd
+++ ui.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file('res://main.tscn')
+
+
+func _on_info_pressed() -> void:
+	get_tree().change_scene_to_file('res://info.tscn')
